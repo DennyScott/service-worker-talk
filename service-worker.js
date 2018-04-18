f10ab182c0f449e6ed55119d4887fe78cc9af019
Denny Scott <dennys@iqmetrix.com>
2018-04-18 15:24:10 -0500

    Handle error

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,17 @@
+const data = [
+  {
+    body: "quia et suscipit↵suscipit recusandae consequuntur expedita et",
+    id: 1,
+    title: "sunt aut facere repellat provident occaecati excepturi optio",
+    userId: 1
+  }
+];
+
 self.addEventListener("fetch", event => {
-  console.log("event happened");
+  console.log("listening for fetch");
+  event.respondWith(
+    fetch(event.request).catch(error => {
+      return new Response(JSON.stringify(data));
+    })
+  );
 });

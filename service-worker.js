8fc89dc1c1be51678c4a9461493ef988ed921156
Denny Scott <dennys@iqmetrix.com>
2018-04-18 17:01:02 -0500

    image cache

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,11 @@ const networkFirstStrategy = event => {
       .then(response => {
         console.log("in response");
         const store = response.clone();
-        caches
-          .open(event.request.url)
-          .then(cache => cache.put(event.request, store));
+        if (!event.request.url.includes("chrome-extension")) {
+          caches
+            .open(event.request.url)
+            .then(cache => cache.put(event.request, store));
+        }
         return response;
       })
       .catch(error => {
@@ -17,23 +19,35 @@ const networkFirstStrategy = event => {
 };
 
 const cacheFirstStrategy = event => {
+  console.log("cache first strategy");
   event.respondWith(
     caches.match(event.request).then(response => {
-      if (!response) {
-        return fetch(event.request).then(fetchResponse => {
+      console.log(response);
+      if (!response.ok) {
+        console.log("no previous cache");
+        return fetch(event.request.url).then(fetchResponse => {
+          console.log("cache the data");
+          console.log(fetchResponse);
           caches
             .open(event.request.url)
-            .then(cache => cache.put(event.request, fetchResponse.clone));
+            .then(cache => cache.put(event.request, fetchResponse.clone()));
           return fetchResponse;
         });
+      } else {
+        console.log("Found offline");
+        return response;
       }
-      return response;
     })
   );
 };
 
 self.addEventListener("fetch", event => {
-  if (event.request.headers.get("Accept").indexOf("image") !== -1) {
+  console.log("listening");
+  console.log(event);
+  if (
+    event.request.headers.get("Accept").indexOf("image") !== -1 &&
+    !event.request.url.includes("localhost")
+  ) {
     cacheFirstStrategy(event);
   } else {
     networkFirstStrategy(event);
